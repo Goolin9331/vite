01ddeb242e77befbf4742572f39628655ff443b8
goolin9331 <goolin93311049@163.com>
2023-04-20 08:35:36 +0800

    add router view

--- src/router/index.ts
+++ src/router/index.ts
@@ -37,7 +37,7 @@ const routeItems: Array<RouteRecordRaw> = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory("/vite/dist/"),
   routes: routeItems,
 });
 /**
